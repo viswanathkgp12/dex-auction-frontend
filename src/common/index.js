c24408bdbfebef10b3ebbebf287fca53dfbeaa77
viswanathkgp12 <viswanath.iit@gmail.com>
2020-06-28 01:36:22 +0530

    new changes

---
+++ src/common/index.js
@@ -0,0 +1,481 @@
+import { getOpByHashTzkt, getContractStorage } from "../utils/api";
+import { sleep } from "../utils/sleep";
+import {
+  isAvailable,
+  connect,
+  getWalletInfo,
+  createInstance,
+  configureAuction,
+} from "../utils/thanos";
+
+/**
+ * --------------------------------
+ * Wallet
+ * --------------------------------
+ */
+
+// Wallet State
+const NOT_CONNECTED = "not-connected";
+const CONNECTING = "connecting";
+const CONNECTED = "connected";
+
+let walletState = NOT_CONNECTED;
+let walletAddress;
+
+export async function checkAvailability() {
+  console.log("Checking thanos wallet availability ...");
+  const available = await isAvailable();
+  if (!available) {
+    alert("Thanos Wallet is not available");
+  }
+}
+
+export async function connectWallet() {
+  console.log("Connecting to RPC ...");
+  walletState = CONNECTING;
+  await connect();
+
+  console.log("Fetch wallet info ...");
+  const { tezos, address } = await getWalletInfo();
+  console.log("Address of wallet: ", address);
+  walletState = CONNECTED;
+  walletAddress = address;
+}
+
+/**
+ * ---------------------------
+ * Auction Contract
+ * ---------------------------
+ */
+
+let contractAddress;
+
+async function createAuctionInstance(assetName, auctionType) {
+  const { err, opHash } = await createInstance(0, assetName, auctionType);
+  $("#chooseAuctionResult").html(`${opHash}`);
+  if (err) {
+    console.log("Error occured");
+    return;
+  }
+
+  console.log("Create Instance succeeded with: ", opHash);
+
+  const pollResult = await pollForAuctionAddress(opHash);
+  console.log("Contract created at: ", pollResult.contractInstance);
+
+  return pollResult;
+}
+
+async function pollForAuctionAddress(opHash, retries = 10) {
+  try {
+    if (!retries) {
+      return {
+        err: "Timeout exceeded",
+        contractInstance: null,
+      };
+    }
+
+    const tzktOpdata = await getOpByHashTzkt(opHash);
+    console.log("TZKT Operation data received: ", tzktOpdata);
+
+    if (
+      tzktOpdata !== null &&
+      tzktOpdata.length > 0 &&
+      tzktOpdata.length == 4
+    ) {
+      const status = tzktOpdata[2].status;
+      if (status === "applied") {
+        return {
+          err: null,
+          contractInstance: tzktOpdata[2].originatedContract.address,
+        };
+      }
+    }
+
+    await sleep(5000);
+    return pollForAuctionAddress(opHash, retries - 1);
+  } catch (error) {
+    console.log(error);
+    return pollForAuctionAddress(opHash, retries - 1);
+  }
+}
+
+// Configure auction
+async function configureAuctionInstance(
+  contractInstance,
+  increment,
+  reservePrice,
+  starttime,
+  waittime
+) {
+  const configureAuctionResult = await configureAuction(
+    contractInstance,
+    increment,
+    reservePrice,
+    starttime,
+    waittime
+  );
+
+  if (configureAuctionResult.err) {
+    console.log("Error occured");
+    return;
+  }
+
+  console.log(
+    "Create Instance succeeded with: ",
+    configureAuctionResult.opHash
+  );
+}
+
+/**
+ * ---------------------
+ * UI Bindings
+ * ----------------------
+ */
+
+export function checkAndSetKeys() {
+  const assetName = localStorage.getItem("assetName");
+  if (!assetName) return;
+
+  $("#auctnProdct").val(assetName);
+  $("li.one").addClass("active");
+  $("li.one").addClass("bold");
+  $("div#one.comnTab").addClass("active");
+
+  auctionType = localStorage.getItem("auctionType");
+  if (!auctionType) return;
+
+  $("#auctionType").val(auctionType);
+
+  if (localStorage.getItem("contractAddress")) {
+    contractAddress = localStorage.getItem("contractAddress");
+  } else {
+    return;
+  }
+
+  $("li.two").addClass("active");
+  $("li.one").removeClass("bold");
+  $("li.two").addClass("bold");
+  $("div#one.comnTab").removeClass("active");
+  $("div#two.comnTab").addClass("active");
+  // $(".tabHead ul li.one").addClass("disabled").css("opacity", 0.5);
+  // $(".tabHead ul li.two").addClass("disabled").css("opacity", 0.5);
+  // $("#chooseAuctionBtn").prop("disabled", true).css("opacity", 0.5);
+
+  const reservePrice = localStorage.getItem("reservePrice");
+  const increment = localStorage.getItem("increment");
+  const datepicker = localStorage.getItem("datepicker");
+  const timepicker = localStorage.getItem("timepicker");
+  const waithour = localStorage.getItem("waithour");
+  const waitmin = localStorage.getItem("waitmin");
+
+  if (!reservePrice) return;
+  $("#reservePrice").val(reservePrice);
+
+  if (!increment) return;
+  $("#increment").val(increment);
+
+  if (!datepicker) return;
+  $("#datepicker").val(datepicker);
+
+  if (!timepicker) return;
+  $("#timepicker").val(timepicker);
+
+  if (!waithour) return;
+  $("#waithour").val(waithour);
+
+  if (!waitmin) return;
+  $("#waitmin").val(waitmin);
+
+  $("li.three").addClass("active");
+  $("li.two").removeClass("bold");
+  $("li.three").addClass("bold");
+  $("div#two.comnTab").removeClass("active");
+  $("div#three.comnTab").addClass("active");
+  // $(".tabHead ul li.three").addClass("disabled").css("opacity", 0.5);
+  // $("#configureAuctionBtn").prop("disabled", true).css("opacity", 0.5);
+}
+
+// Fill asset name and click
+$(".sbmt.first").on("click", function () {
+  const assetName = $("#auctnProdct").val();
+
+  if (assetName.length < 1) {
+    $(".aucPro.error").show();
+    $("#auctnProdct").focus();
+    return;
+  }
+
+  localStorage.setItem("assetName", assetName);
+
+  $(".tabHead ul li.two").addClass("active");
+  $(".tabHead ul li.two").addClass("bold");
+
+  $("#two").addClass("active");
+  $("#one").removeClass("active");
+  $(".one").removeClass("bold");
+});
+
+export function hideSlider() {
+  // UI
+  $("body").removeClass("openSlide");
+  $(".menuBox ul li.prodct").removeClass("active");
+  $("body").addClass("overlaLoader");
+}
+
+/**
+ * -------------------------
+ * Choose Auction
+ * -------------------------
+ */
+
+window.chooseAuction = async function () {
+  const assetName = $("#auctnProdct").val();
+
+  localStorage.setItem("auctionType", auctionType);
+
+  $("#chooseAuctionBtn").prop("disabled", true).css("opacity", 0.5);
+  $(".ldngAuctn").show();
+  $(".creatdAuctn").show();
+
+  const result = await createAuctionInstance(assetName, auctionType);
+
+  if (result.err) {
+    $("#chooseAuctionError").html(result.err);
+    $("#chooseAuctionBtn").prop("disabled", false).css("opacity", 1);
+    $(".ldngAuctn").hide();
+    $(".creatdAuctn").hide();
+    return;
+  }
+
+  contractAddress = result.contractInstance;
+  if (!contractAddress) {
+    $("#chooseAuctionBtn").prop("disabled", false).css("opacity", 1);
+    return;
+  }
+
+  localStorage.setItem("contractAddress", contractAddress);
+
+  // UI
+  $(".tabHead ul li.two").removeClass("bold");
+
+  $(".tabHead ul li.three").addClass("active");
+  $(".tabHead ul li.three").addClass("bold");
+
+  $("#three").addClass("active");
+  $("#two").removeClass("active");
+
+  $(".tabHead ul li.one").addClass("disabled").css("opacity", 0.5);
+  $(".tabHead ul li.two").addClass("disabled").css("opacity", 0.5);
+  $(".creatdAuctn").hide();
+  $(".ldngAuctn").hide();
+};
+
+/**
+ * ------------------------------
+ * Configure Auction
+ * ------------------------------
+ */
+
+function getAuctionType() {
+  const auctionTypeMapping = {
+    english: "English Auction",
+    dutch: "Dutch Auction",
+    vickery: "Vickery",
+    sealed_bid: "Sealed Bid",
+  };
+
+  return auctionTypeMapping[auctionType];
+}
+
+window.configureAuction = async function () {
+  $("#tab3-auction-type").html(getAuctionType());
+
+  const reservePrice = $("#reservePrice").val();
+  const increment = $("#increment").val();
+  const datepicker = $("#datepicker").val();
+  const timepicker = $("#timepicker").val();
+  const waithour = $("#waithour").val();
+  const waitmin = $("#waitmin").val();
+
+  if (isNaN(reservePrice)) {
+    $(".Reserve.error").show();
+    return;
+  } else {
+    $(".Reserve.error").hide();
+  }
+
+  if (isNaN(increment)) {
+    $(".Increment.error").show();
+    return;
+  } else {
+    $(".Increment.error").hide();
+  }
+
+  if (!timepicker) {
+    $(".Time.error").show();
+    return;
+  } else {
+    $(".Time.error").hide();
+  }
+
+  if (!datepicker) {
+    $(".Date.error").show();
+    return;
+  } else {
+    $(".Date.error").hide();
+  }
+
+  localStorage.setItem("reservePrice", reservePrice);
+  localStorage.setItem("increment", increment);
+  localStorage.setItem("datepicker", datepicker);
+  localStorage.setItem("timepicker", timepicker);
+  localStorage.setItem("waithour", waithour);
+  localStorage.setItem("waitmin", waitmin);
+
+  const monthName = datepicker.split(" ")[0];
+  const month = (() => {
+    const mapping = {
+      jan: "01",
+      feb: "02",
+      mar: "03",
+      apr: "04",
+      may: "05",
+      jun: "06",
+      jul: "07",
+      aug: "08",
+      sep: "09",
+      oct: "10",
+      nov: "11",
+      dec: "12",
+    };
+    return mapping[monthName.toLowerCase()];
+  })();
+  const date = datepicker.split(" ")[1].split(",")[0];
+  const year = datepicker.split(" ")[2];
+
+  let starttime;
+  const tzOffset = new Date().getTimezoneOffset();
+
+  const symbol = tzOffset < 0 ? "+" : "-";
+  let tzOffsetHours = Math.floor(Number(Math.abs(tzOffset) / 60));
+  const tzOffsetMins = Math.abs(tzOffset) - tzOffsetHours * 60;
+
+  tzOffsetHours = tzOffsetHours < 10 ? "0" + tzOffsetHours : tzOffsetHours;
+
+  const isAM = timepicker.includes("AM");
+  console.log(timepicker.slice(0, -3))
+  if (isAM) {
+    let hour = timepicker.split(":")[0];
+    const min = timepicker.slice(0, -3).split(":")[1];
+
+    starttime = `${year}-${month}-${date}T${hour}:${min}:00${symbol}${tzOffsetHours}:${tzOffsetMins}`;
+  } else {
+    let hour = timepicker.split(":")[0];
+    const min = timepicker.slice(0, -3).split(":")[1];
+
+    hour = Number(hour) + 12;
+    starttime = `${year}-${month}-${date}T${hour}:${min}:00${symbol}${tzOffsetHours}:${tzOffsetMins}`;
+  }
+
+  const waittime = Number(waithour) * 60 * 60 + Number(waitmin) * 60;
+
+  console.log(contractAddress, increment, reservePrice, starttime, waittime);
+
+  await configureAuctionInstance(
+    contractAddress,
+    increment,
+    reservePrice,
+    starttime,
+    waittime
+  );
+  const storage = await getContractStorage(contractAddress);
+  setStorage(storage);
+  await submitForm();
+
+  hideSlider();
+  $(".tabHead ul li.three").addClass("disabled").css("opacity", 0.5);
+  $("#configureAuctionBtn").prop("disabled", true).css("opacity", 0.5);
+};
+
+let auctionType;
+
+window.setAuctionType = function (type) {
+  auctionType = type;
+};
+
+async function submitForm() {
+  $("form#auction-details-form").submit(function (e) {
+    e.preventDefault();
+  });
+}
+
+function setStorage(instanceStorageDetails) {
+  let assetId = "";
+  let startTime = "";
+  let waitTime = "";
+  let auctionParams = {};
+  instanceStorageDetails.children.forEach((element) => {
+    switch (element.name) {
+      // generic auction details
+      case "asset_id":
+        assetId = element.value;
+        break;
+      case "start_time":
+        startTime = element.value;
+        break;
+      case "wait_time":
+        waitTime = element.value;
+        break;
+      // English auction params
+      case "current_bid":
+        auctionParams.currentBid = element.value;
+        break;
+      case "min_increase":
+        auctionParams.minIncrease = element.value;
+        break;
+      case "highest_bidder":
+        auctionParams.highestBidder = element.value;
+        break;
+      // Dutch auction params
+      case "current_price":
+        auctionParams.currentPrice = element.value;
+        break;
+      case "reserve_price":
+        auctionParams.reservePrice = element.value;
+        break;
+      // Sealed bid auction params
+      case "participation_fee":
+        auctionParams.participationFee = element.value;
+        break;
+      case "highest_bid":
+        auctionParams.highestBid = element.value;
+        break;
+      case "highest_bidder":
+        auctionParams.highestBidder = element.value;
+        break;
+      default:
+      //
+    }
+  });
+
+  $("#assetId").val(assetId);
+  $("#auctionType").val(auctionType);
+  $("#startTime").val(startTime);
+  $("#waitTime").val(waitTime);
+  $("#auctionParams").val(JSON.stringify(auctionParams));
+  $("#userPubKey").val(walletAddress);
+  $("#contractAddress").val(contractAddress);
+}
+
+$(document).ready(function () {
+  // Hide animations
+  $(".ldngAuctn").hide();
+  $(".creatdAuctn").hide();
+
+  isAvailable().then((available) => {
+    if (available) {
+      $(".thanos-banner").hide();
+    }
+  });
+});
---
+++ src/page-auction/main.js
@@ -0,0 +1,202 @@
+import { getOpByHashTzkt, getAuctions } from "../utils/api";
+import { sleep } from "../utils/sleep";
+import { startAuction, bid } from "../utils/thanos";
+import { connectWallet, checkAvailability, checkAndSetKeys } from "../common";
+
+/**
+ * ---------------------------
+ * Auction Contract
+ * ---------------------------
+ */
+
+async function createBid(contractAddress, amount) {
+  await startAuction(contractAddress);
+  const { err, opHash } = await bid(contractAddress, amount);
+  if (err) {
+    console.log("Error occured");
+    return;
+  }
+
+  console.log("Bid for auction succeeded with: ", opHash);
+
+  //   return poll(opHash);
+}
+
+async function poll(opHash, retries = 10) {
+  try {
+    if (!retries) {
+      return {
+        err: "Timeout exceeded",
+        contractInstance: null,
+      };
+    }
+
+    const tzktOpdata = await getOpByHashTzkt(opHash);
+    console.log("TZKT Operation data received: ", tzktOpdata);
+
+    if (
+      tzktOpdata !== null &&
+      tzktOpdata.length > 0 &&
+      tzktOpdata.length == 4
+    ) {
+      const status = tzktOpdata[2].status;
+      if (status === "applied") {
+        return {
+          err: null,
+          contractInstance: tzktOpdata[2].originatedContract.address,
+        };
+      }
+    }
+
+    retries--;
+
+    await sleep(5000);
+    return poll(opHash, retries);
+  } catch (error) {
+    return poll(opHash, retries);
+  }
+}
+
+/**
+ * ---------------------
+ * UI Bindings
+ * ----------------------
+ */
+
+$("#prodct").on("click", async function () {
+  // Check Thanos Availability
+  await checkAvailability();
+  await connectWallet();
+
+  checkAndSetKeys();
+
+  // Open slider
+  $("body").addClass("openSlide");
+  $(".menuBox ul li.prodct").addClass("active");
+});
+
+window.shortlistAuction = async function shortlistAuction() {
+  console.log("---------------");
+  // Check Thanos Availability
+  await connectWallet();
+
+  const contractAddress = "KT19uwQwQjeqgH9tyrzWFEHeiqBVChVVfZB1";
+  const amount = 1e-6;
+
+  await createBid(contractAddress, amount);
+};
+
+// Auctions populate
+
+let upcomingAuctionsCount = 0,
+  ongoingAuctionsCount = 0,
+  completedAuctionsCount = 0;
+
+function getAuctionType(auctionType) {
+  const auctionTypeMapping = {
+    english: "English Auction",
+    dutch: "Dutch Auction",
+    vickery: "Vickery",
+    sealed_bid: "Sealed Bid",
+  };
+
+  return auctionTypeMapping[auctionType];
+}
+
+function populateAuctions(auctionJson) {
+  const auctionStatus = auctionJson.auctionStatus;
+  const auctionName = auctionJson.assetName;
+  const auctionType = getAuctionType(auctionJson.auctionType);
+  const auctionDescription = auctionJson.assetDescription;
+  const auctionReservePrice = auctionJson.auctionParams.currentBid;
+  const auctionIncrement = auctionJson.auctionParams.minIncrease;
+  const auctionStartDate = new Date(auctionJson.startTime);
+
+  const diffTime = Math.abs(auctionStartDate - Date.now());
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  const rem = diffTime - diffDays * (1000 * 60 * 60 * 24);
+  const diffHours = Math.ceil(rem / (1000 * 60 * 60));
+  const timeLeft = `${diffDays} day ${diffHours} hrs`;
+
+  const waitTime = auctionJson.waitTime;
+  const waitHours = Math.floor(waitTime / (60 * 60));
+  const waitMins = Math.ceil((waitTime - waitHours * (60 * 60)) / 60);
+
+  const auctionDuration = `${waitHours} hr ${waitMins} mins`;
+  const owner = auctionJson.owner;
+
+  const auctionItemCard = `
+    <div class="prod-card">
+      <div class="lt auctionImage"><img src="src/images/Image-8.jpg" /></div>
+      <div class="rt">
+          <div class="left">
+            <h1 class="auctionName">${auctionName}</h1>
+            <h2 class="auctionType">${auctionType}</h2>
+            <h3>
+                Owner:
+                <span class="owner">
+                  <a class="carthagelink" href="https://carthage.tzkt.io/${owner}" target="blank">${owner}</a>
+                </span>
+            </h3>
+            <div class="paragrph auctionDescription">
+                <p>${auctionDescription}</p>
+            </div>
+          </div>
+          <div class="right">
+            <h3>
+                Reserve Price <span class="auctionReservePrice">${auctionReservePrice} XTZ</span>
+            </h3>
+            <h4>
+                Min. Increment : <span class="auctionIncrement">${auctionIncrement} XTZ</span>
+            </h4>
+            <ul>
+                <li>
+                  <span>Start Date <cite class="timeLeft">${timeLeft}</cite></span>
+                  <span class="auctionStartDate">${auctionStartDate}</span>
+                </li>
+                <li>
+                  <span>Round Duration</span>
+                  <span class="auctionDuration">${auctionDuration}</span>
+                </li>
+            </ul>
+            <input
+                type="button"
+                class="btn shortlistbtn"
+                value="Shortlist"
+                onclick="shortlistAuction()"
+                />
+          </div>
+      </div>
+    </div>
+    `;
+
+  if (auctionStatus === "upcoming") {
+    upcomingAuctionsCount++;
+    $("#upcoming-count").html(upcomingAuctionsCount);
+    $("#upcoming-list").append(auctionItemCard);
+  } else if (auctionStatus === "ongoing") {
+    ongoingAuctionsCount++;
+    $("#ongoing-count").html(upcomingAuctionsCount);
+    $("#ongoing-list").append(auctionItemCard);
+  } else if (auctionStatus === "completed") {
+    completedAuctionsCount++;
+    $("#completed-count").html(upcomingAuctionsCount);
+    $("#completed-list").append(auctionItemCard);
+  }
+}
+
+async function updateAuctionData() {
+  upcomingAuctionsCount = 0;
+  ongoingAuctionsCount = 0;
+  completedAuctionsCount = 0;
+
+  const auctions = await getAuctions();
+
+  auctions.forEach((auction) => {
+    populateAuctions(auction);
+  });
+}
+
+$(document).ready(function () {
+  updateAuctionData();
+});
